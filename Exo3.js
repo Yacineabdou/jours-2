e3ec07dd5b40f23c7ed7d9c4c850a356da632f12
yacine <issay584@email.com>
2024-09-25 04:23:25 +0100

    commit 3

---
+++ Exo3.js
@@ -0,0 +1,21 @@
+/*---------------------Exo:3---------------------------*/
+//------------------challenge 1
+let amour = " L'amour est la meilleure chose au monde. Certains ont trouvé l'amour et d'autres le cherchent encore"
+console.log(amour.match("amour"));
+
+//------------------challenge 2
+let imposible = "Vous ne pouvez pas terminer une phrase par parce que parce que parce que est une conjonction"
+console.log(imposible.match("parce que"));
+
+let sentence1
+const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+if(sentence === sentence){
+    console.log(sentence1 = "I am a teacher, and I love teaching. There is nothing; as more rewarding as educating and empowering people. I found teaching more interesting than any other jobs. does this motivate you to be a teacher! this 30DaysOfJavaScript is also the result of love of teaching");
+}
+
+//------------------challenge 4
+let salaireGagnerPArMOI = 5000
+let bonnusAnnuel = 10000
+let coursEnLigneParMoi = 15000
+let revenuAnnuelTotal  
+console.log(revenuAnnuelTotal = salaireGagnerPArMOI + bonnusAnnuel + coursEnLigneParMoi);
